8c476d81fa7e3fafde665f91bf506ad1da1b3401
Emre Bican <emrebican11@hotmail.com>
2022-09-23 14:34:48 +0300

    Navbar component completed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,31 @@
 import styles from '../styles/Navbar.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
+import { RiArrowDownSLine } from 'react-icons/ri'
 
 const Navbar = () => {
   return (
     <nav className={styles.container}>
-      <div>
-        <Image src="/images/logo.png" width={36} height={36} />
-        <span className={styles.title}>modal.cards</span>
+      <div className={styles.wrapper}>
+        <div className={styles.logo}>
+          <Image src="/images/logo.png" width={36} height={36} />
+          <span className={styles.title}>modal.cards</span>
+        </div>
+
+        <div className={styles.links}>
+          <Link href="/">
+            <a className={styles.icon}>
+              solutions <RiArrowDownSLine />
+            </a>
+          </Link>
+          <Link href="/">product tour</Link>
+          <Link href="/">showcase</Link>
+          <Link href="/">pricing</Link>
+        </div>
       </div>
-      <ul>
-        <li>
-          <Link href="#">solutions</Link>
-        </li>
-        <li>
-          <Link href="#">product tour</Link>
-        </li>
-        <li>
-          <Link href="#">showcase</Link>
-        </li>
-        <li>
-          <Link href="#">pricing</Link>
-        </li>
-      </ul>
-      <div>
-        <button>sign in</button>
-        <button>try for free</button>
+      <div className={styles.buttons}>
+        <button>Sign in</button>
+        <button className={styles.right_btn}>Try for free</button>
       </div>
     </nav>
   )
